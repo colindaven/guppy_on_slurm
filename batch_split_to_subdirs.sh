8632f3c56cacd5eecfdf4686453aa96db0656072
Colin Davenport <davenport.colin@mh-hannover.de>
2021-05-19 16:26:40 +0200

    Make docs clearer

--- batch_split_to_subdirs.sh
+++ batch_split_to_subdirs.sh
@@ -1,4 +1,6 @@
-# Colin 2019_03, from https://unix.stackexchange.com/questions/285538/how-can-i-move-files-into-subdirectories-by-count
-# 50 is the default number to move into subdirs. Try also 20
- n=0; for f in *.fast5; do d="subdir$((n++ / 20))"; mkdir -p "$d"; mv -- "$f" "$d/$f"; done
+# Colin Davenport 2019_03, from https://unix.stackexchange.com/questions/285538/how-can-i-move-files-into-subdirectories-by-count
+# 3 is the default number of (4000 read) FAST5 files to move into subdirs.
+# Each subdir is processed independently by the runbatch_singularity_guppy.sh script
+# Usage: bash batch_split_to_subdirs.sh (in a folder containing FAST5 files)
+ n=0; for f in *.fast5; do d="subdir$((n++ / 3))"; mkdir -p "$d"; mv -- "$f" "$d/$f"; done
 
--- runbatch_singularity_guppy.sh
+++ runbatch_singularity_guppy.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
-## Colin Davenport, July 2020 -  May 2021
+# Colin Davenport, July 2020 -  May 2021
 # Note - singularity does NOT work with sbatch, but does with srun. Therefore no additional sbatch script needed.
-# See guppy page on Dokuwiki for full usage
-# Now optionally uses Singularity - needed on 20.04 cluster
+# Now optionally uses Singularity - previously needed on our 20.04 cluster
+# Alternatively, you can use the native Guppy, as there is now a version for Ubuntu 20.04
 # This version uses config and models from bonito/rerio - see bottom
-
 # For large multi-fast5 files (after 2019_03)
+# Usage: a) create subdir directories bash batch_split_to_subdirs.sh
+# Usage: b) set parameters and paths
+# Usage: c) bash runbatch_singularity_guppy.sh
 
-# use Singularity container version of guppy (just a file so can be used by an entire cluster if on an NFS filesystem)
-# build a Singularity container from docker: singularity build guppy453.sif docker://genomicpariscentre/guppy
+# use Singularity container version of guppy 
+# Optional: build Sing container from docker container with: singularity build guppy453.sif docker://genomicpariscentre/guppy
 #guppy_basecaller="singularity exec /mnt/ngsnfs/tools/guppy/guppy361.sif guppy_basecaller"
 #guppy_basecaller="singularity exec /mnt/ngsnfs/tools/guppy/guppy440.sif guppy_basecaller"
 guppy_basecaller="singularity exec /mnt/ngsnfs/tools/guppy/guppy453.sif guppy_basecaller"
 
-# use standard guppy - not recommended
+# use standard guppy if desired
 #guppy_basecaller=guppy_basecaller
 
 # Calibration ref
@@ -27,40 +29,41 @@ model=/mnt/ngsnfs/tools/guppy/rerio/basecall_models/res_dna_r941_min_dUhac.jsn
 
 for i in $(ls -d subdir*)
 
-        do
-	# set -c 28 for 24 cores, or -c 10 for 8 cores, otherwise runs too hard (2 IO threads)
+    do
+	# set -c 28 for 24 cores, or -c 10 for 8 cores, otherwise runs too hard (~ 2 IO threads)
 	# set queue, jobname, max time
 	reservedCpus=10
 	cpus=8
-	settings="-p lowprio -J guppy_singularity --mem=20000"
+	queue="lowprio"
+	settings="-p $queue -J guppy_sing --mem=20000"
 
 	echo $i
 
-        # Uncomment one of the following srun lines as appropriate
+    # Uncomment one of the following srun lines as appropriate
 
-        #echo "DEBUG: " $guppy_basecaller
+    #echo "DEBUG: " $guppy_basecaller
 
-        ##############
-        # slow, higher accuracy
-        ##############
+    ##############
+    # slow, higher accuracy mode
+    ##############
 
-	# high accuracy, 7x + slower (40+ hours)
+	# high accuracy, 7x + slower 
 	srun -c $reservedCpus $settings $guppy_basecaller -i $i  -s $i.guppy --cpu_threads_per_caller 1 --num_callers $cpus -c $config -m $model &
 	# high accuracy with calibration strand detection/filtering
 	#nohup srun -c $reservedCpus $settings $guppy_basecaller -i $i  -s $i.guppy --cpu_threads_per_caller 1 --num_callers $cpus -c $config -m $model --calib_detect --calib_reference $calref
 
 
 	##############
-	# fast, lower accuracy, 7x + faster (6hours?)
+	# fast, lower accuracy mode, 7x + faster
 	##############
 
-        # fast, lower accuracy, 7x + faster (6hours?)
+    # fast, lower accuracy, 7x + faster (6hours?)
 	#nohup srun -c $reservedCpus $settings $guppy_basecaller -i $i  -s $i.guppy --cpu_threads_per_caller 1 --num_callers $cpus -c $config -m $model
 	# fast with calibration strand detection/filtering
 	# nohup srun -c $reservedCpus $settings  $guppy_basecaller -i $i  -s $i.guppy --cpu_threads_per_caller 1 --num_callers $cpus -c $config -m $model --calib_detect --calib_reference $calref
 
-       # just print help
-#	nohup srun -c $reservedCpus $settings  $guppy_basecaller
+    # just print help (for testing)
+	#nohup srun -c $reservedCpus $settings  $guppy_basecaller
 
 
 
@@ -70,7 +73,7 @@ done
 
 
 
-# from rerio project
+# These are just examples for paths to models from rerio project on our filesystem
 #/mnt/ngsnfs/tools/guppy/rerio/basecall_models/res_dna_r103_min_crf_v030.cfg
 #/mnt/ngsnfs/tools/guppy/rerio/basecall_models/res_dna_r103_min_flipflop_v001.cfg
 #/mnt/ngsnfs/tools/guppy/rerio/basecall_models/res_dna_r103_prom_modbases_5mC_v001.cfg
