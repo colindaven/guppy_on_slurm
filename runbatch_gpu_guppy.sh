940d0a5004cf3e56a545596a1fbbcf43f88cfad6
Colin Davenport <davenport.colin@mh-hannover.de>
2021-08-09 11:51:00 +0200

    Simplify gpu script

--- runbatch_gpu_guppy.sh
+++ runbatch_gpu_guppy.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 # Colin Davenport, July 2020 -  June 2021
-# This version uses a local guppy version with GPU, so does _not_ use Singularity or multiple servers ie. via SLURM
+# This version uses a local guppy version with GPU, so does _not_ use Singularity, does _not_ use multiple servers ie. via SLURM
+# Run it on your GPU server only!
 
 # This version uses config and models from bonito/rerio - see bottom
+# It expects a folder containing lots of fast5 files (i.e. don't use the batch_split_to_subdirs.sh script as with the CPU/SLURM version)
 # Usage: a) set parameters and paths
 # Usage: b) bash runbatch_gpu_guppy.sh input_fast5_dir
+
 
 
 # use standard guppy if desired
 guppy_basecaller=guppy_basecaller
 
-# Calibration ref
-calref=../../../../lager2/rcug/seqres/contaminants/2020_02/ont_calib/DNA_CS.fasta
+# Calibration ref (optional)
+#calref=../../../../lager2/rcug/seqres/contaminants/2020_02/ont_calib/DNA_CS.fasta
 
 # bonito rerio high accuracy models and configs
 # See bottom for all models
@@ -22,39 +25,23 @@ model=/mnt/ngsnfs/tools/guppy/rerio/basecall_models/res_dna_r941_min_dUhac.jsn
 # Use the first argument supplied on the command line for the input directory containing fast5 files.
 i=$1
 
-	echo "Starting guppy on dir: "$i
+echo "Starting guppy on dir: "$i
 
-	# set -c 28 for 24 cores, or -c 10 for 8 cores, otherwise runs too hard (~ 2 IO threads)
-	# set queue, jobname, max time
-	reservedCpus=10
-	cpus=8
-	queue="lowprio"
-	settings="-p $queue -J guppy_sing --mem=20000"
-	# for nvidia A100, 40GB ram
-	gpu_params='--compress_fastq --num_callers 14 --gpu_runners_per_device 8 --chunks_per_runner 512 --chunk_size 3000'
-	out_dir="guppy_out"
+# for nvidia A100, 40GB ram
+gpu_params='--compress_fastq --num_callers 14 --gpu_runners_per_device 8 --chunks_per_runner 512 --chunk_size 3000'
+out_dir="guppy_out"
 
 
-    #echo "DEBUG: " $guppy_basecaller
+#echo "DEBUG: " $guppy_basecaller
 
-    ##############
-    # slow, higher accuracy mode
-    ##############
+##############
+# slow, higher accuracy mode
+##############
 
-	## WARNING - not tested with version 507 yet. Super accuracy available TODO
+## WARNING - not tested with version 507 yet. Super accuracy available TODO
 
-	# high accuracy, 7x + slower
-	nohup guppy_basecaller -i $i -s $out_dir gpu_params -x "cuda:0" -c $config -m $model &
-
-
-    # just print help (for testing)
-	#nohup srun -c $reservedCpus $settings  $guppy_basecaller
-
-
-
-#done
-
-
+# high accuracy, 7x + slower
+nohup guppy_basecaller -i $i -s $out_dir gpu_params -x "cuda:0" -c $config -m $model &
 
 
 
