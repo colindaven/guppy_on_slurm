63e502a51c2178200ec94daae67be99380f66d19
Dr. Colin Davenport <davenport.colin@mh-hannover.de>
2022-01-10 17:18:42 +0100

    Add barcoder

---
+++ run_guppy_barcoder.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+# Colin Davenport, Jan 2022
+
+# It expects a folder containing lots of fastq files 
+# Usage: a) set parameters and paths
+# Usage: b) bash runbatch_gpu_guppy.sh input_fastq_dir (NOT fast5 files)
+
+
+
+#barcoder
+#guppy_barcoder="guppy_barcoder"
+guppy_barcoder="singularity exec /mnt/ngsnfs/tools/guppy/guppy601.sif guppy_barcoder"
+
+
+# Use the first argument supplied on the command line for the input directory containing fast5 files.
+i=$1
+out_dir="barcodes_out"
+
+# If the user doesn't supply any arguments then exit
+if [ $# -eq 0 ]; then
+    echo "Supply folder of fastqs (eg pass) from guppy_basecaller as argument"
+    exit 0
+fi
+
+
+echo "Starting guppy on dir: "$i
+
+#echo "DEBUG: " $guppy_basecaller
+
+#############
+# barcoder only, non-gpu
+##############
+
+# You don't need to enter --barcode_kits
+barcode_params='--compress_fastq --barcode_kits EXP-NBD104 --trim_barcodes'
+#barcode_params='--compress_fastq --barcode_kits EXP-NBD104 --trim_barcodes --allow_inferior_barcodes --detect_mid_strand_barcodes'
+#barcode_params='--compress_fastq --trim_barcodes'
+
+
+# barcoder - CPU only, not GPU
+rm nohup.out
+nohup $guppy_barcoder -i $i -s $out_dir $barcode_params &
+
+
+
+
+
+
+
+
+
+# More settings
+
+#  --require_barcodes_both_ends    Reads will only be classified if there is a
+#                                  barcode above the min_score at both ends of
+#                                  the read.
+#  --allow_inferior_barcodes       Reads will still be classified even if both
+#                                  the barcodes at the front and rear (if
+#                                  applicable) were not the best scoring
+#                                  barcodes above the min_score.
+#  --detect_mid_strand_adapter     Detect adapter sequences within reads.
+#  --detect_mid_strand_barcodes    Search for barcodes through the entire length
+#                                  of the read.
+#  --min_score_mid_barcodes arg    Minimum score for a barcode to be detected in
+#                                  the middle of a read.
+
--- runbatch_gpu_guppy.sh
+++ runbatch_gpu_guppy.sh
@@ -11,7 +11,13 @@
 
 
 # use standard guppy if desired
-guppy_basecaller=guppy_basecaller
+#guppy_basecaller=guppy_basecaller
+guppy_basecaller="singularity exec /mnt/ngsnfs/tools/guppy/guppy601.sif guppy_basecaller"
+
+#barcoder
+guppy_barcoder="singularity exec /mnt/ngsnfs/tools/guppy/guppy601.sif guppy_barcoder"
+
+
 
 # Calibration ref (optional)
 #calref=../../../../lager2/rcug/seqres/contaminants/2020_02/ont_calib/DNA_CS.fasta
@@ -19,6 +25,7 @@ guppy_basecaller=guppy_basecaller
 # bonito rerio high accuracy models and configs
 # See bottom for all models
 config=/mnt/ngsnfs/tools/guppy/rerio/basecall_models/res_dna_r941_min_dUhac.cfg
+#config=/mnt/ngsnfs/tools/guppy/rerio/basecall_models/res_dna_r9.4.1_e8.1_sup_v033
 #config=dna_r9.4.1_450bps_hac.cfg
 model=/mnt/ngsnfs/tools/guppy/rerio/basecall_models/res_dna_r941_min_dUhac.jsn
 
@@ -28,9 +35,10 @@ i=$1
 echo "Starting guppy on dir: "$i
 
 # for nvidia A100, 40GB ram
-gpu_params='--compress_fastq --num_callers 14 --gpu_runners_per_device 8 --chunks_per_runner 512 --chunk_size 3000'
+#gpu_params='--compress_fastq --num_callers 14 --gpu_runners_per_device 8 --chunks_per_runner 512 --chunk_size 3000'
+gpu_params='--nv --compress_fastq --num_callers 14 --gpu_runners_per_device 8 --chunks_per_runner 512 --chunk_size 3000'
+barcode_params='--compress_fastq --barcode_kits "EXP-NBD104" --trim_barcodes'
 out_dir="guppy_out"
-
 
 #echo "DEBUG: " $guppy_basecaller
 
@@ -41,7 +49,11 @@ out_dir="guppy_out"
 ## WARNING - not tested with version 507 yet. Super accuracy available TODO
 
 # high accuracy, 7x + slower
-nohup guppy_basecaller -i $i -s $out_dir gpu_params -x "cuda:0" -c $config -m $model &
+#nohup $guppy_basecaller -i $i -s $out_dir $gpu_params -x "cuda:0" -c $config -m $model &
+
+
+# barcoder - CPU only, not GPU
+nohup $guppy_barcoder -i $i -s $out_dir $barcode_params -c $config -m $model &
 
 
 
@@ -86,3 +98,79 @@ nohup guppy_basecaller -i $i -s $out_dir gpu_params -x "cuda:0" -c $config -m $m
 #/mnt/ngsnfs/tools/guppy/rerio/basecall_models/res_dna_r941_prom_modbases_5mC_v001.jsn
 #/mnt/ngsnfs/tools/guppy/rerio/basecall_models/res_rna2_r941_min_flipflop_v001.jsn
 
+
+#ls -1 /mnt/ngsnfs/tools/guppy/rerio/basecall_models/
+#adapter_scaling_dna_r10.3_prom.jsn
+#adapter_scaling_dna_r9.4.1_min.jsn
+#adapter_scaling_dna_r9.4.1_prom.jsn
+#barcoding
+#res_dna_r103_min_crf_v030
+#res_dna_r103_min_crf_v030.cfg
+#res_dna_r103_min_crf_v030.jsn
+#res_dna_r103_min_crf_v032
+#res_dna_r103_min_crf_v032.cfg
+#res_dna_r103_min_crf_v032.jsn
+#res_dna_r103_min_flipflop_v001
+#res_dna_r103_min_flipflop_v001.cfg
+#res_dna_r103_min_flipflop_v001.jsn
+#res_dna_r103_prom_modbases_5mC_v001
+#res_dna_r103_prom_modbases_5mC_v001.cfg
+#res_dna_r103_prom_modbases_5mC_v001.checkpoint
+#res_dna_r103_prom_modbases_5mC_v001.jsn
+#res_dna_r103_prom_rle_v001
+#res_dna_r103_prom_rle_v001.cfg
+#res_dna_r103_prom_rle_v001.jsn
+#res_dna_r103_q20ea_crf_v033
+#res_dna_r103_q20ea_crf_v033.cfg
+#res_dna_r103_q20ea_crf_v033.jsn
+#res_dna_r103_q20ea_crf_v034
+#res_dna_r103_q20ea_crf_v034.cfg
+#res_dna_r103_q20ea_crf_v034.jsn
+#res_dna_r9.4.1_e8.1_fast_v033
+#res_dna_r9.4.1_e8.1_hac_v033
+#res_dna_r9.4.1_e8.1_sup_v033
+#res_dna_r941_min_crf_v031
+#res_dna_r941_min_crf_v031.cfg
+#res_dna_r941_min_crf_v031.jsn
+#res_dna_r941_min_crf_v032
+#res_dna_r941_min_crf_v032.cfg
+#res_dna_r941_min_crf_v032.jsn
+#res_dna_r941_min_dUfast.cfg
+#res_dna_r941_min_dUfast.jsn
+#res_dna_r941_min_dUfast_v001
+#res_dna_r941_min_dUhac.cfg
+#res_dna_r941_min_dUhac.jsn
+#res_dna_r941_min_dUhac_v001
+#res_dna_r941_min_flipflop_v001
+#res_dna_r941_min_flipflop_v001.cfg
+#res_dna_r941_min_flipflop_v001.jsn
+#res_dna_r941_min_modbases_5mC_5hmC_CpG_v001
+#res_dna_r941_min_modbases_5mC_5hmC_CpG_v001.cfg
+#res_dna_r941_min_modbases_5mC_5hmC_CpG_v001.jsn
+#res_dna_r941_min_modbases_5mC_5hmC_v001
+#res_dna_r941_min_modbases_5mC_5hmC_v001.cfg
+#res_dna_r941_min_modbases_5mC_5hmC_v001.checkpoint
+#res_dna_r941_min_modbases_5mC_5hmC_v001.jsn
+#res_dna_r941_min_modbases_5mC_CpG_v001
+#res_dna_r941_min_modbases_5mC_CpG_v001.cfg
+#res_dna_r941_min_modbases_5mC_CpG_v001.jsn
+#res_dna_r941_min_modbases_5mC_v001
+#res_dna_r941_min_modbases_5mC_v001.cfg
+#res_dna_r941_min_modbases_5mC_v001.checkpoint
+#res_dna_r941_min_modbases_5mC_v001.jsn
+#res_dna_r941_min_modbases-all-context_v001
+#res_dna_r941_min_modbases-all-context_v001.cfg
+#res_dna_r941_min_modbases-all-context_v001.jsn
+#res_dna_r941_min_rle_v001
+#res_dna_r941_min_rle_v001.cfg
+#res_dna_r941_min_rle_v001.jsn
+#res_dna_r941_prom_modbases_5mC_CpG_v001
+#res_dna_r941_prom_modbases_5mC_CpG_v001.cfg
+#res_dna_r941_prom_modbases_5mC_CpG_v001.jsn
+#res_dna_r941_prom_modbases_5mC_v001
+#res_dna_r941_prom_modbases_5mC_v001.cfg
+#res_dna_r941_prom_modbases_5mC_v001.checkpoint
+#res_dna_r941_prom_modbases_5mC_v001.jsn
+#res_rna2_r941_min_flipflop_v001
+#res_rna2_r941_min_flipflop_v001.cfg
+#res_rna2_r941_min_flipflop_v001.jsn
